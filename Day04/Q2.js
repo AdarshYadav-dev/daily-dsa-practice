54557f16b5a94da89fcf1a570390d1a22e531359
AdarshYadav-dev <adarsh98215@gmail.com>
2025-12-23 22:47:44 +0530

    Solve and Day 04 Question 2 Daily DSA practice questions (arrays)

---
+++ Day04/Q2.js
@@ -0,0 +1,20 @@
+//  Find the largest and smallest element in an array  
+
+
+let arr = [234, 5432, 234, 5432, 234];
+
+let largest = arr[0];
+let smallest = arr[0];
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+        largest = arr[i];
+    }
+
+    if(arr[i] < smallest){
+        smallest = arr[i]
+    }
+}
+
+console.log("badi value:", largest);
+console.log("choti value is:", smallest);
