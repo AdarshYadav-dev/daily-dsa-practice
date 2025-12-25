841ba18033488650f30673851ba1272ada9c149c
AdarshYadav-dev <adarsh98215@gmail.com>
2025-12-25 20:22:35 +0530

    Solve Day 04

--- Day04/Q3.js
+++ Day04/Q3.js
@@ -1,18 +1,18 @@
 //  Find the second largest element in an array  
 
 
-let arr = [1234,5432,234,5432,234];
+let arr = [1234, 5432, 234, 543, 23425, 76543];
 
-let largest = arr[0];
-let secondLargest = -Infinity;
+let larest_value = arr[0];
+let secound_largest_value = arr[0];
 
-for (let i = 0; i < arr.length; i++){
-    if(arr[i] > largest){
-        secondLargest = largest;
-        largest = arr[i];
-    } else if(arr[i] > secondLargest && arr[i] != largest){
-        secondLargest = arr[i];
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > larest_value) {
+        secound_largest_value = larest_value;
+        larest_value = arr[i];
+    } else if (arr[i] < larest_value && arr[i] > secound_largest_value) {
+        secound_largest_value = arr[i]
     }
 }
 
-console.log("second badi value is : ", secondLargest);
+console.log("secound badi value is : ", secound_largest_value)
