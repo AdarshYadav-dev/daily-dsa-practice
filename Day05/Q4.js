f4e858e7bace3e70cef9af3959a26c125b479749
AdarshYadav-dev <adarsh98215@gmail.com>
2025-12-25 21:08:38 +0530

    solve Q4

--- Day05/Q4.js
+++ Day05/Q4.js
@@ -1,1 +1,11 @@
 // Reverse an array without using the built-in `reverse()` method 
+
+let arr = [23456,76543,234567876,5434567,8765432345];
+
+let result = [];
+
+for(let i = arr.length-1; i>= 0; i--){
+    result[result.length] = arr[i]
+}
+
+console.log(result)
